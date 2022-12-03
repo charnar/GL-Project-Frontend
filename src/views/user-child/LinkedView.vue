0cff244baa3cabd099ae600801aa955d736b0403
charnar <charn.arunkit@gmail.com>
2022-12-03 21:21:03 +0700

    working with backend

--- src/views/user-child/LinkedView.vue
+++ src/views/user-child/LinkedView.vue
@@ -4,19 +4,30 @@
 
 <script>
 import { mapGetters } from "vuex";
+import { API_URL } from "@/configs.js";
+import axios from "axios";
 export default {
+  computed: {
+    ...mapGetters(["getSessionID"]),
+  },
+
   async mounted() {
     try {
       const linkedAccountID = this.$route.query["openid.identity"].slice(37);
       console.log(this.getSessionID);
+      const payload = {
+        session_id: this.getSessionID,
+        library_type: "STEAM",
+        library_api_key: linkedAccountID,
+      };
+
+      console.log(payload);
+      const response = await axios.post(`${API_URL}/register-library`, payload);
+      console.log(response.data);
       this.$router.push("/user/accounts");
     } catch (err) {
       console.error("Something went wrong...");
     }
-  },
-
-  computed: {
-    ...mapGetters(["getSessionID"]),
   },
 };
 </script>
